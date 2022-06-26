f108c829fd4aea76bdc136ff12705c73edec1db2
Jackey Yang <jyang@castlepointanime.com>
2022-06-26 13:26:02 -0500

    changed styling/layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Nav from "../components/Nav/Nav.js"
+import Hero from "../components/Hero/Hero.js"
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-dark-bluegray text-white ">
+    <div className="h-screen mx-10vw text-white ">
       <Nav />
-      Hello World
+      <Hero />
     </div>
   )
 }

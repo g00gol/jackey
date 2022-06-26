b84a0e2d179f69ebcdae6b298b606afe8773c106
Jackey Yang <jyang@castlepointanime.com>
2022-06-26 14:25:22 -0500

    starting mobile-first development

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,13 +1,31 @@
+import Image from "next/image"
+import { useState } from "react"
+
 export default function Nav() {
+	const [isShow, setIsShow] = useState(false)
+
 	return (
-		<div className="bg-transparent w-full text-lg">
+		<div className="bg-transparent w-full text-sm md:text-lg">
 			<div className="py-6 flex justify-between align-center">
+
 				<a href="#">Jackey Yang</a>
-				<span className="space-x-8">
+				<span className="hidden md:block space-x-8">
 					<a href="">Projects</a>
 					<a href="">About</a>
 					<a href="">Contact</a>
 				</span>
+
+				<div className="md:hidden">
+					{!isShow ? (
+						<button onClick={() => setIsShow(!isShow)}>
+							<Image src="/hamburger.svg" height={15} width={25} />
+						</button>
+					) : (
+						<button onClick={() => setIsShow(!isShow)}>
+							close
+						</button>
+					)}
+				</div>
 			</div>
 		</div>
 	)

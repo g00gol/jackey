dcb3370ddafc844523a30f75132bf3de8ef73113
Jackey Yang <jyang@castlepointanime.com>
2022-06-26 14:25:06 -0500

    starting mobile-first development

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,12 +1,12 @@
 export default function Hero() {
 	return (
-		<div className="mt-32">
-			<div className="text-4xl">
+		<div className="mt-16">
+			<div className="text-xl md:text-4xl">
 				Hi, I&apos;m Jackey Yang, <br />
 				an aspiring <mark className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-violet to-skyblue">
 					Software Developer</mark>.
 			</div>
-			<div className="text-3xl text-gray-dark">
+			<div className="text-lg md:text-3xl text-gray-dark">
 				I currently study Computer Science <br />
 				at Stevens Institute of Technology.
 			</div>

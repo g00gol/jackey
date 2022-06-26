6c141b0495b15f12f227fa2f18713331c2c26b39
Jackey Yang <jyang@castlepointanime.com>
2022-06-25 22:29:24 -0500

    added custom colors bc im cool like that

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      "white": "#F5F5F7",
+      "dark-bluegray": "#2E3035"
+    },
     extend: {},
   },
   plugins: [],

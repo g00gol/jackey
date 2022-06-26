f22985960a865e0d6f867e220d1b86082ecfc917
Jackey Yang <jyang@castlepointanime.com>
2022-06-26 13:25:43 -0500

    updated nav

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,7 +1,14 @@
 export default function Nav() {
 	return (
-		<div className="bg-transparent min-w-screen w-screen">
-			<a href="#">Jackey Yang</a>
+		<div className="bg-transparent w-full text-lg">
+			<div className="py-6 flex justify-between align-center">
+				<a href="#">Jackey Yang</a>
+				<span className="space-x-8">
+					<a href="">Projects</a>
+					<a href="">About</a>
+					<a href="">Contact</a>
+				</span>
+			</div>
 		</div>
 	)
 }

ff83ed713a35cf96f7a034b08670d410a455da91
Jackey Yang <jyang@castlepointanime.com>
2022-06-26 13:25:32 -0500

    created hero section

---
+++ components/Hero/Hero.js
@@ -0,0 +1,15 @@
+export default function Hero() {
+	return (
+		<div className="mt-32">
+			<div className="text-4xl">
+				Hi, I'm Jackey Yang, <br />
+				an aspiring <mark className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-violet to-skyblue">
+					Software Developer</mark>.
+			</div>
+			<div className="text-3xl text-gray-dark">
+				I currently study Computer Science <br />
+				at Stevens Institute of Technology.
+			</div>
+		</div>
+	)
+}

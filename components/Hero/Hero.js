d4756b66b7aebe682c32d050097863c85e1eb9f8
Jackey Yang <jyang@castlepointanime.com>
2022-06-26 13:33:25 -0500

    fixed escape character deploy issue

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -2,7 +2,7 @@ export default function Hero() {
 	return (
 		<div className="mt-32">
 			<div className="text-4xl">
-				Hi, I'm Jackey Yang, <br />
+				Hi, I&apos;m Jackey Yang, <br />
 				an aspiring <mark className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-violet to-skyblue">
 					Software Developer</mark>.
 			</div>

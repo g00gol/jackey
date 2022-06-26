f5516b14a4385bf6cdd9ba086b946d6598c21af7
Jackey Yang <jyang@castlepointanime.com>
2022-06-25 22:33:12 -0500

    import nav

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Nav from "../components/Nav/Nav.js"
+
 export default function Home() {
   return (
-    <div>
+    <div className="w-screen h-screen bg-dark-bluegray text-white ">
+      <Nav />
       Hello World
     </div>
   )

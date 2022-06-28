8c5a28f84e5175818842818063ec46a84b8b8790
g00gol <jackey2870@gmail.com>
2022-06-28 16:28:49 -0500

    changed .js to .jsx for future clarity

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Nav from "../components/Nav/Nav.jsx";
+import Hero from "../components/Hero/Hero.jsx";
+
+export default function Home() {
+  return (
+    <div className="h-screen mx-10vw text-white ">
+      <Nav />
+      <Hero />
+    </div>
+  );
+}
